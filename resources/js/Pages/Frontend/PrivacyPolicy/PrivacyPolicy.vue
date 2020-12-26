55dac3e023fca16f7f26cec43798f8aa9e756d12
ahsanmahmood <aoneahsan@gmail.com>
2020-12-26 14:19:58 +0500

    updated

--- resources/js/Pages/Frontend/PrivacyPolicy/PrivacyPolicy.vue
+++ resources/js/Pages/Frontend/PrivacyPolicy/PrivacyPolicy.vue
@@ -443,13 +443,11 @@
 import { InertiaLink } from "@inertiajs/inertia-vue";
 import Header from "./../Header/Header";
 import Footer from "./../Footer/Footer";
-import PageLoader from "./../PageLoader/PageLoader";
 export default {
   components: {
     InertiaLink,
     Header,
     Footer,
-    PageLoader,
   },
 };
 </script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -11,6 +11,12 @@ import VueParticles from "vue-particles";
 import VueScrollTo from "vue-scrollto";
 import VueLoading from "vue-element-loading";
 import VueYoutube from "vue-youtube";
+import { InertiaProgress } from '@inertiajs/progress'
+
+// custom global components
+import PageLoader from "./GlobalComponents/PageLoader/PageLoader";
+
+// IMPORTS ENDS ABOVE
 
 Vue.mixin({ methods: { route } });
 Vue.use(InertiaApp);
@@ -18,8 +24,16 @@ Vue.use(InertiaForm);
 Vue.use(PortalVue);
 Vue.use(VueParticles);
 Vue.use(VueScrollTo);
-Vue.component("VueLoading", VueLoading);
-Vue.use(VueYoutube, { global: true, componentId: "youtube" });
+Vue.component("VueLoading", VueLoading); // https://www.npmjs.com/package/vue-element-loading
+Vue.use(VueYoutube, { global: true, componentId: "youtube" }); // https://www.npmjs.com/package/vue-youtube
+
+// Custom Defined Components
+Vue.component("PageLoader", PageLoader);
+
+// COMPOENENTS DECLERATION ENDS ABOVE
+
+// Inertia Page Loader Progress bar
+InertiaProgress.init()
 
 const app = document.getElementById("app");
 
